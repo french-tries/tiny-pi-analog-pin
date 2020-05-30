883eceddaf7e3c478ad691a196197342cd7f4f97
french-tries <none>
2020-05-29 21:19:49 -0400

    Multiple message values; decoupled transitions using pattern matching

--- Program.cs
+++ Program.cs
@@ -1,90 +1,112 @@
 ﻿using System;
 
-// rollover
+
+// skip state
+// multiple communications
 
 namespace driver_csharp
 {
 	public static class Config
 	{
+		public const int IdBits = 2;
 		public const int MessageBits = 10;
 	}
-	public interface State
+	public abstract class State
 	{
-		State levelChange(bool level, long tick);
+		protected State(int id = 0) {
+			Id = id;
+		}
+
+		public int Id { get; }
+		public abstract State levelChange(bool rising, long tick);
 	}
 
 	public sealed class InitState : State
 	{
-		public State levelChange(bool level, long tick) => 
-			new IntervalState(tick);
+		public InitState(Func<State, bool, long, State> onSuccess) {
+			OnSuccess = onSuccess;
+		}
+
+		private Func<State, bool, long, State> OnSuccess { get; }
+		public override State levelChange(bool rising, long tick) => 
+			OnSuccess(this, rising, tick);
 	}
 
 	public sealed class IntervalState : State
 	{
-		public IntervalState(long tick)
+		public IntervalState(Func<State, bool, long, State> onSuccess, long tick) : base()
 		{
+			OnSuccess = onSuccess;
 			StartTick = tick;
 		}
+
+		private Func<State, bool, long, State> OnSuccess { get; }
 		public long StartTick { get; }
 
-		public State levelChange(bool level, long tick) => 
-			new DataState(tick - StartTick, tick, Config.MessageBits, true, 0);
+		public long getInterval(long tick) => tick - StartTick;
+
+		public override State levelChange(bool rising, long tick) => 
+			OnSuccess(this, rising, tick);
 	}
 
 	public sealed class DataState : State
 	{
-		public DataState(long interval, long startTick, int remaining, bool ignoreFirst, int value)
+		public DataState(Func<State, bool, long, State> onSuccess, 
+			int id, long interval, long startTick, int remaining) : 
+			this(onSuccess, id, interval, startTick, remaining, true, 0)
+		{}
+		public DataState(Func<State, bool, long, State> onSuccess, int id,
+			long interval, long startTick, int remaining, bool ignoreFirst, int value)  : base(id)
 		{
+			OnSuccess = onSuccess;
 			Interval = interval;
 			StartTick = startTick;
 			Remaining = remaining;
 			IgnoreFirst = ignoreFirst;
 			Value = value;
 		}
+
+		public DataState With(long? startTick = null,
+			int? remaining = null, bool? ignoreFirst = null, int? value = null) =>
+			new DataState(OnSuccess, Id, Interval, 
+				startTick ?? StartTick, remaining ?? Remaining, false, value ?? Value);
+
+		private Func<State, bool, long, State> OnSuccess { get; }
+
 		public long Interval { get; }
 		public long StartTick { get; }
 		public int Remaining { get; }
 		public bool IgnoreFirst { get; }
 		public int Value { get; }
 
-		public int countBits(long tick)
-		{
-			int numBits = Convert.ToInt32((tick - StartTick) / Interval);
-
-			if ((tick - StartTick) % Interval > Interval /5*4)
-				++numBits;
-			if (IgnoreFirst)
-				--numBits;
-
-			return numBits;
+		public long nextTick(long tick) => (tick - StartTick < Interval) ? Interval : (StartTick + Interval);
+		public int updateValue(bool rising) {
+			if (IgnoreFirst || rising)
+				return Value;
+			return Value | 1 << (Remaining-1);
 		}
 
-		public DataState updateTimestamp(long tick) => 
-			new DataState(Interval, tick, Remaining, IgnoreFirst, Value);
-		public DataState transmitValue() {
-			Console.WriteLine(Value);
+		public DataState processBits(bool rising, long tick) 
+		{
+			if (tick - StartTick < Interval/5)
+				return With(startTick: tick);
+			if (Remaining <= 0)
+				return this;
+			if (tick - StartTick > Interval/5*4)
+				if (IgnoreFirst)
+					return With(startTick: nextTick(tick), ignoreFirst: false).processBits(rising, tick);
+				else
+					return With(startTick: nextTick(tick), remaining: Remaining-1,
+						value: updateValue(rising)).processBits(rising, tick);
 			return this;
 		}
-		public InitState reinit() => new InitState();
 
-		public DataState updateValue(int numBits) 
+		public override State levelChange(bool rising, long tick)
 		{
-			if (numBits <= 0 || Remaining <= 0)
-				return this;
-
-			return new DataState(Interval, StartTick, Remaining-1, false, Value | 1 << (Remaining-1)).updateValue(numBits-1);
-		}
-
-		public DataState shiftRemaining(int numBits) => new DataState(Interval, StartTick, Remaining-numBits, false, Value);
-
-		public State levelChange(bool level, long tick)
-		{
-			var newState = level ?
-				updateTimestamp(tick).shiftRemaining(countBits(tick)) :
-				updateTimestamp(tick).updateValue(countBits(tick));
+			var newState = processBits(rising, tick);
 
 			if(newState.Remaining <= 0)
-				return newState.transmitValue().reinit();
+				return newState.OnSuccess(newState, rising, tick);
 			else
 				return newState;
 		}
@@ -92,20 +114,45 @@ namespace driver_csharp
 
 	class Program
 	{
+		static Func<State, bool, long, State> transitions = (State s, bool rising, long tick) => {
+			switch (s){
+				case InitState i:
+					return new IntervalState(transitions, tick);
+				case IntervalState i:
+					return new DataState(transitions, 0, i.getInterval(tick), tick, Config.IdBits);
+				case DataState d when d.Id == 0:
+					Console.WriteLine(d.Value);
+					return new DataState(transitions, 1, d.Interval, d.StartTick, Config.MessageBits, false, 0).levelChange(rising, tick);
+				case DataState d when d.Id == 1:
+					Console.WriteLine(d.Value);
+					return new InitState(transitions);
+				default:
+					Console.WriteLine("error");
+					return new InitState(transitions);
+			}
+		};
 		static void Main(string[] args)
 		{
-			State state = new InitState();
+			State state = new InitState(transitions);
 			state.levelChange(false, 0)
-				.levelChange(true, 10)
-				.levelChange(false, 30)
-				.levelChange(true, 110)
-				.levelChange(false, 120); // 513
+				.levelChange(true, 10) // interval
+				.levelChange(false, 20)
+				.levelChange(true, 30)
+				.levelChange(false, 40) // 1
+				.levelChange(true, 130)
+				.levelChange(false, 140); // 1
 
-			State state2 = new InitState();
-			state.levelChange(true, 0)
-				.levelChange(false, 10)
-				.levelChange(true, 110)
-				.levelChange(false, 120); // 1
+			State state2 = new InitState(transitions);
+			state2.levelChange(true, 0)
+				.levelChange(false, 10) //interval
+				.levelChange(true, 130)	// 0
+				.levelChange(false, 140); // 1
+
+			State state3 = new InitState(transitions);
+			state3.levelChange(false, 0)
+				.levelChange(true, 10) //interval
+				.levelChange(false, 140)	// 3
+				.levelChange(true, 150); // 1023
 		}
 	}
 }
